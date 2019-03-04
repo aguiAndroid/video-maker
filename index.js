9c50fe5e2db4383e78a3d3c7a0946bdafab1e0a5
Aguinaldo Barbosa <aguinaldo@MacBook-Pro-de-Aguinaldo.local>
2019-03-04 15:46:45 -0300

    add function retun prefix

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const readline = require('readline-sync')
 function start() {
     const content = {}
     content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnSearchPrefix()
 
     function askAndReturnSearchTerm() {
         return readline.question('Type a Wikipedia search term:')
+    }
+
+    function askAndReturnSearchPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixTextgi
     }
     console.log(content)
     
